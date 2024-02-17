19241c84477c637bb3c3056d38246e736e301b73
mustafaasoud <mustafa.asoud.2014@gmail.com>
2024-02-17 12:42:14 +0300

    me

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, avoid_print, prefer_const_constructors, unused_import
+// ignore_for_file: prefer_typing_uninitialized_variables, avoid_print, prefer_const_constructors, unused_import, must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' as dateint;
@@ -19,16 +19,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
 
   final String title;
-
+  String? div;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }

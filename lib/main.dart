71446464d0d0140d671f85eb16165e0346796cfb
mustafaasoud <mustafa.asoud.2014@gmail.com>
2024-02-17 11:57:38 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, avoid_print, prefer_const_constructors, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' as dateint;
+import 'package:gap/gap.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  DateTime date = DateTime.now();
+  late var formattedDate;
+  late int selectedRadio;
+  @override
+  void initState() {
+    super.initState();
+    selectedRadio = 1;
+    formattedDate = dateint.DateFormat('dd-mm-yyyy').format(date);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
